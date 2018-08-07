3a160c06ca8c1422fc9359f6fba001ac91c705d9
Patricia Carter <patricia4194@gmail.com>
2018-08-07 15:07:36 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.first.shift
+  array[0].shift
 end
 
 def retrieve_first_element_from_array(array)
