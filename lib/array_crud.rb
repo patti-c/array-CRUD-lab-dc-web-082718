5b5cc247be7584f92108fb5f7c601080affe3590
Patricia Carter <patricia4194@gmail.com>
2018-08-07 15:07:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array[0].shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[0].shift
+  
 end
 
 def retrieve_first_element_from_array(array)
